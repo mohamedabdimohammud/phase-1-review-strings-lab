a04da81005eb6059afac9f1af09d135f9df78944
mohamedabdimohammud <mmohammud@outlook.com>
2023-03-13 21:48:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+// Define the `currentUser` variable
+const currentUser = "John Doe";
+
+// Define the `welcomeMessage` variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the `excitedWelcomeMessage` variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the `shortGreeting` variable
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+// Run Mocha tests
+mocha.run();
